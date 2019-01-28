0b95fd76b1bdf579f525be0b082602d095f407a5
Niklas <niklasvdh@gmx.ch>
2019-01-28 21:13:12 +0100

    v.0.2

--- particle.js
+++ particle.js
@@ -1,32 +1,73 @@
 let particle = {
   position: null,
   radius: 0,
+  mass: 0,
+  gravity: null,
   color: null,
   velocity: null,
-  gravity: null,
-  create(x, y, radius, speed, angle, gravity) {
+  create(x, y, mass, gravity, speed, angle) {
     let object = Object.create(this);
     object.position = vector.create(x, y);
-    object.radius = radius;
+    object.radius = mass / Math.PI;
+    object.mass = mass;
+    object.gravity = gravity;
     object.color = getRandomColor();
     object.velocity = vector.create(0, 0);
     object.velocity.setMagnitude(speed);
     object.velocity.setAngle(angle);
-    object.gravity = vector.create(0, gravity || 0);
     return object;
   },
   update() {
     this.position.addTo(this.velocity);
-    this.velocity.addTo(this.gravity);
   },
   accelerate(acceleration) {
     this.velocity.addTo(acceleration);
+  },
+  angleTo(otherParticle) {
+    return Math.atan2(
+      otherParticle.position.getY() - this.position.getY(),
+      otherParticle.position.getX() - this.position.getX()
+    );
+  },
+  distanceTo(otherParticle) {
+    let dx = otherParticle.position.getX() - this.position.getX(),
+      dy = otherParticle.position.getY() - this.position.getY();
+    return Math.sqrt(dx * dx + dy * dy);
+  },
+  gravitateTo(otherParticle) {
+    let gravity = vector.create(0, 0),
+      distance = this.distanceTo(otherParticle);
+    //mass
+    gravity.setMagnitude(
+      (this.gravity *
+        ((otherParticle.getMass() * this.mass) / (distance * distance))) /
+        this.mass
+    );
+    gravity.setAngle(this.angleTo(otherParticle));
+    this.velocity.addTo(gravity);
+  },
+  merge(otherParticle) {
+    let newVelocity = this.velocity;
+    return this.create(
+      this.position.getX(),
+      this.position.getY(),
+      this.getMass() + otherParticle.getMass(),
+      this.gravity,
+      newVelocity.getMagnitude(),
+      newVelocity.getAngle()
+    );
   },
   setRadius(newRadius) {
     this.radius = newRadius;
   },
   getRadius() {
     return this.radius;
+  },
+  setMass(newMass) {
+    this.mass = newMass;
+  },
+  getMass() {
+    return this.mass;
   }
 };
 
--- simulation.js
+++ simulation.js
@@ -7,18 +7,53 @@ window.onload = () => {
   const c = canvas.getContext("2d");
 
   let particles = [];
-  let numberParticles = 300;
+  let numberParticles = 1;
+  const gravitationalConstant = 0.8;
+
+  particles.push(
+    particle.create(
+      canvasWidth / 2,
+      canvasHeight / 2,
+      200,
+      gravitationalConstant,
+      0,
+      0
+    )
+  );
+  particles.push(
+    particle.create(
+      canvasWidth / 3,
+      canvasHeight / 2,
+      50,
+      gravitationalConstant,
+      0.8,
+      80
+    )
+  );
+  particles.push(
+    particle.create(
+      canvasWidth / 3.5,
+      canvasHeight / 2,
+      10,
+      gravitationalConstant,
+      1.5,
+      80
+    )
+  );
 
   canvas.addEventListener("mousedown", event => {
     for (let i = 0; i < numberParticles; i++) {
+      let mass = Math.random() * 30 + 10;
       particles.push(
         particle.create(
-          event.layerX,
-          event.layerY,
-          Math.random() * 5 + 1,
-          Math.random() * 3 + 1,
-          Math.random() * Math.PI * 2,
-          0.05
+          event.layerX, // + Math.random() * 400 - 200,
+          event.layerY, // + Math.random() * 400 - 200,
+          mass,
+          gravitationalConstant,
+          //Math.random() * 2 - 1,
+          0,
+          //Math.random() * Math.PI * 2
+          0
         )
       );
     }
@@ -27,33 +62,63 @@ window.onload = () => {
   update();
 
   function update() {
-    console.log(particles.length, "particles being calculated!");
     c.clearRect(0, 0, canvasWidth, canvasHeight);
-
-    particles.forEach(particle => {
-      particle.update();
-      c.beginPath();
-      c.fillStyle = particle.color;
-      c.arc(
-        particle.position.getX(),
-        particle.position.getY(),
-        particle.getRadius(),
-        0,
-        Math.PI * 2
-      );
-      c.fill();
-    });
-
-    for (var i = particles.length - 1; i >= 0; i -= 1) {
-      var p = particles[i];
-      if (
-        p.position.getX() - p.radius > canvasWidth ||
-        p.position.getX() + p.radius < 0 ||
-        p.position.getY() - p.radius > canvasHeight ||
-        p.position.getY() + p.radius < 0
-      ) {
-        particles.splice(i, 1);
+    if (particles.length > 0) {
+      let particlesToRemove = [];
+      let particlesToMerge = [];
+      for (let i = 0; i < particles.length; i++) {
+        let particle = particles[i];
+        if (particles.length > 1) {
+          for (let j = 0; j < particles.length; j++) {
+            if (particle != particles[j]) {
+              if (
+                particle.position.getX() - particle.radius <
+                  particles[j].position.getX() - particles[j].radius &&
+                particle.position.getX() + particle.radius >
+                  particles[j].position.getX() + particles[j].radius &&
+                particle.position.getY() - particle.radius <
+                  particles[j].position.getY() - particles[j].radius &&
+                particle.position.getY() + particle.radius >
+                  particles[j].position.getY() + particles[j].radius
+              ) {
+                particlesToMerge.push({ a: particle, b: particles[j] });
+                particlesToRemove.push(particle);
+                particlesToRemove.push(particles[j]);
+              } else {
+                particle.gravitateTo(particles[j]);
+              }
+            }
+          }
+        }
+        particle.update();
+        c.beginPath();
+        c.fillStyle = particle.color;
+        c.arc(
+          particle.position.getX(),
+          particle.position.getY(),
+          particle.getRadius(),
+          0,
+          Math.PI * 2
+        );
+        c.fill();
+        if (
+          particle.position.getX() - particle.radius > canvasWidth ||
+          particle.position.getX() + particle.radius < 0 ||
+          particle.position.getY() - particle.radius > canvasHeight ||
+          particle.position.getY() + particle.radius < 0
+        ) {
+          particlesToRemove.push(particle);
+        } else {
+          particles[i] = particle;
+        }
       }
+      particles = particles.filter(particle => {
+        return !particlesToRemove.includes(particle);
+      });
+      particlesToMerge.forEach(pair => {
+        let mergedParticle = pair.a.merge(pair.b);
+        particles.push(mergedParticle);
+      });
     }
 
     requestAnimationFrame(update);
